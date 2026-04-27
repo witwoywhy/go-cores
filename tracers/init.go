1b2dafa4b2eb61f8e93a952fe4e3c017b2c3a2d2
Nuttawit Butsara <nuttawit@gsbitmanagement.co.th>
2026-04-27 16:02:04 +0700

    update: tracer:

--- tracers/init.go
+++ tracers/init.go
@@ -3,33 +3,36 @@ package tracers
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/spf13/viper"
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	ot "go.opentelemetry.io/otel/trace"
 )
 
-var (
-	TraceProvider *trace.TracerProvider
-	TraceContext  context.Context
-	TraceCancel   context.CancelFunc
-	Trace         ot.Tracer
-)
+var Tracer *TracerInfo
 
-func InitTracer(url string) {
-	exporter, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+type TracerInfo struct {
+	Provider *trace.TracerProvider
+	Trace    ot.Tracer
+	Context  context.Context
+	Cancel   context.CancelFunc
+}
+
+func Init(url string) {
+	exporter, err := otlptracehttp.New(context.Background(), otlptracehttp.WithEndpointURL(url))
 	if err != nil {
 		panic(fmt.Errorf("failed when init otel exporter: %v", err))
 	}
 
-	TraceProvider = trace.NewTracerProvider(
+	provider := trace.NewTracerProvider(
 		trace.WithBatcher(exporter),
 		trace.WithResource(
 			resource.NewWithAttributes(
@@ -40,22 +43,33 @@ func InitTracer(url string) {
 		),
 	)
 
-	TraceContext, TraceCancel = context.WithCancel(context.Background())
+	otel.SetTracerProvider(provider)
+	ctx, cancel := context.WithCancel(context.Background())
 
-	otel.SetTracerProvider(TraceProvider)
-	Trace = otel.Tracer(viper.GetString("app.name"))
+	Tracer = &TracerInfo{
+		Provider: provider,
+		Trace:    otel.Tracer(viper.GetString("app.name")),
+		Context:  ctx,
+		Cancel:   cancel,
+	}
 }
 
-func ShutDown() error {
-	if TraceContext != nil {
-		ctx, cancel := context.WithTimeout(TraceContext, 5*time.Second)
-		defer cancel()
+var onceShutdown sync.Once
 
-		if err := TraceProvider.Shutdown(ctx); err != nil {
-			return err
+func Shutdown() error {
+	var err error
+	onceShutdown.Do(func() {
+		if Tracer != nil && Tracer.Context != nil {
+			ctx, cancel := context.WithTimeout(Tracer.Context, 5*time.Second)
+			defer cancel()
+
+			err = Tracer.Provider.Shutdown(ctx)
+
+			Tracer.Cancel()
 		}
-
-		TraceCancel()
+	})
+	if err != nil {
+		return err
 	}
 
 	return nil
