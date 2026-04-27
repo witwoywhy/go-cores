bddc7aa6727f7f44e69fd6efc19b66c7250774a2
Nuttawit Butsara <nuttawit@gsbitmanagement.co.th>
2026-04-27 09:53:27 +0700

    feat: update vipers

--- vipers/config.go
+++ vipers/config.go
@@ -1,9 +1,11 @@
 package vipers
 
+import "os"
+
 var (
-	fileNameConfig      = "config"
-	fileExtensionConfig = "yaml"
-	pathConfig          = "./configs"
+	pathConfig          = getENVWithDefault("VIPER_PATH_CONFIG", "./config")
+	fileNameConfig      = getENVWithDefault("VIPER_FILENAME_CONFIG", "config")
+	fileExtensionConfig = getENVWithDefault("VIPER_FILE_EXTENSION_CONFIG", "yaml")
 )
 
 func SetFileNameConfig(config string) {
@@ -16,4 +18,13 @@ func SetFileExtensionConfig(config string) {
 
 func SetPathConfig(config string) {
 	pathConfig = config
+}
+
+func getENVWithDefault(key, def string) string {
+	env := os.Getenv(key)
+	if env == "" {
+		return def
+	}
+
+	return env
 }
