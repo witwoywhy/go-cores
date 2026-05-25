44f4a7d2c00e604b8b8f97d7e6f6d881f0f7b10c
Nuttawit Butsara <nuttawit@gsbitmanagement.co.th>
2026-05-25 16:50:33 +0700

    feat: add echos app

---
+++ echos/example/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+	"github.com/witwoywhy/go-cores/contexts"
+	"github.com/witwoywhy/go-cores/echos"
+	"github.com/witwoywhy/go-cores/errs"
+	"github.com/witwoywhy/go-cores/logger"
+	"github.com/witwoywhy/go-cores/vipers"
+)
+
+func init() {
+	vipers.Init()
+}
+
+func main() {
+	app := echos.New[contexts.RouteContext]()
+
+	app.UseMiddleware(app.Log())
+	app.UseMiddleware(app.Error())
+
+	app.Register(
+		http.MethodGet,
+		"/logger",
+		app.WithLogger(func(ctx *echo.Context, l logger.Logger) {
+			l.Info("HANDLE WITH LOGGER")
+			ctx.JSON(http.StatusOK, "hello")
+		}),
+	)
+
+	app.UseMiddleware(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c *echo.Context) error {
+			c.Set("rctx", &contexts.RouteContext{})
+			return next(c)
+		}
+	})
+
+	app.Register(
+		http.MethodPost,
+		"/context",
+		app.WithRouteContext(func(ctx *echo.Context, rctx *contexts.RouteContext, l logger.Logger) {
+			l.Info("HANDLE WITH ROUTE CONTEXT")
+			ctx.JSON(http.StatusOK, map[string]string{"hello": "world"})
+		}),
+	)
+
+	app.Register(
+		http.MethodGet,
+		"/400",
+		func(ctx *echo.Context) error {
+			return errs.NewBadRequestError()
+		},
+	)
+
+	app.ListenAndServe(func() {
+		fmt.Println("execute close func")
+	})
+}
